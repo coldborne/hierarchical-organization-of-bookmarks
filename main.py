c7864adb04e50bb6b822a3aa28dc33ba98421ea3
coldborne <coldborneVV@gmail.com>
2024-03-20 08:54:14 +0300

    Изменил реализацию парсинга закладок браузера, теперь данные сериализуются в объекты классов для дальнейшей удобной работы + в файл bookmarks.json записываются обработанные данные Сохранил простой вариант также, если необходимо будет получить простой json файл с закладками браузера в формате {name, url}

--- main.py
+++ main.py
@@ -1,29 +1,52 @@
 import json
 
 
-def extract_bookmarks_using_dfs(bookmarks_file_path, output_file_path):
+class Bookmark:
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+
+
+class Folder:
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+
+
+def process_bookmarks(node):
+    if node['type'] == 'url':
+        return Bookmark(node.get('name'), node.get('url'))
+    elif node['type'] == 'folder':
+        folder = Folder(node.get('name'))
+
+        for child in node.get('children', []):
+            folder.children.append(process_bookmarks(child))
+
+        return folder
+
+
+def serialize_structure(node):
+    if isinstance(node, Bookmark):
+        return {'type': 'url', 'name': node.name, 'url': node.url}
+    elif isinstance(node, Folder):
+        children = [serialize_structure(child) for child in node.children]
+        return {'type': 'folder', 'name': node.name, 'children': children}
+
+
+def extract_and_reorganize_bookmarks(bookmarks_file_path, output_file_path):
     with open(bookmarks_file_path, 'r', encoding='utf-8') as file:
         bookmarks_data = json.load(file)
 
-        def dfs(nodes):
-            for node in nodes:
-                if node['type'] == 'url':
-                    extracted_bookmarks.append({'name': node.get('name'), 'url': node.get('url')})
-                elif node['type'] == 'folder' and 'children' in node:
-                    dfs(node['children'])
+    root = bookmarks_data['roots']['bookmark_bar']
+    processed_root = process_bookmarks({'type': 'folder', 'name': 'Root', 'children': root['children']})
 
-        extracted_bookmarks = []
+    reorganized_data = serialize_structure(processed_root)
 
-        for root_key, root_value in bookmarks_data['roots'].items():
-            if root_key == 'bookmark_bar':
-                if 'children' in root_value:
-                    dfs(root_value['children'])
-
-        with open(output_file_path, 'w', encoding='utf-8') as file:
-            json.dump(extracted_bookmarks, file, indent=4, ensure_ascii=False)
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        json.dump(reorganized_data, file, indent=4, ensure_ascii=False)
 
 
 bookmarks_file_path = 'path_to_your_BookmarksFile'
-output_file_path = 'extracted_bookmarks.json'
+output_file_path = 'bookmarks.json'
 
-extract_bookmarks_using_dfs(bookmarks_file_path, output_file_path)
+extract_and_reorganize_bookmarks(bookmarks_file_path, output_file_path)
